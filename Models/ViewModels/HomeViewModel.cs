e87c358652fb051efedfd19311c782811a9e4298
Andre Lillhannus <fasthuntlol@gmail.com>
2022-01-13 14:36:14 +0100

    update

---
+++ Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager_WPF_SQL.Models.ViewModels
+{
+    internal class HomeViewModel
+    {
+    }
+}
--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CustomerManager_WPF_SQL.Data;
+using CustomerManager_WPF_SQL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,26 @@ namespace CustomerManager_WPF_SQL.Services
 {
     public class CustomerService
     {
-        private readonly SqlConte
+        private readonly SqlContext _context = new SqlContext();
+
+        public void CreateCustomer(string firstname, string lastname, string email, string phonenumber, int addressId)
+        {
+            var customer = _context.Customers.Where(x => x.Email == email).FirstOrDefault();
+            if (customer == null)
+            {
+                _context.Customers.Add(new Customer { FirstName = firstname, LastName = lastname, Email = email, PhoneNumber = phonenumber, AddressId = addressId });
+                _context.SaveChanges();
+            }
+        } 
+
+        public void CreateAddress(string streetname, string postalcode, string city, string country)
+        {
+            var address = _context.Addresses.Where(x => x.StreetName == streetname && x.PostalCode == postalcode && x.City == city && x.Country == country).FirstOrDefault();
+            if (address == null)
+            {
+                _context.Addresses.Add(new Address { StreetName = streetname, PostalCode = postalcode, City = city, Country = country });
+                _context.SaveChanges();
+            }
+        }
     }
 }
