b83a8e4cc65fac46f0ad6de46fa998b86c0a81aa
Andre Lillhannus <fasthuntlol@gmail.com>
2022-01-13 11:55:04 +0100

    update

---
+++ Services/CustomerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager_WPF_SQL.Services
+{
+    public class CustomerService
+    {
+        private readonly SqlConte
+    }
+}
